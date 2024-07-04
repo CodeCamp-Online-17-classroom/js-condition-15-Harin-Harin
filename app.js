04847cf915520c486133bce49dff9d7bb2931c8e
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-05 01:36:08 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let browser = prompt('Browser?').toLocaleLowerCase()
+
+if(browser == 'edge'){
+      alert(`"You've got the Edge!"`)
+      }
+    else if(browser == 'chrome' || browser == 'firefox'|| browser == 'safari'|| browser == 'opera'){
+      alert('Okay we support these browsers too');
+    }
+    else{
+      alert('We hope that this page looks ok!');
+  }
